6742a6221afb5eb553b4d2f2c2a66798eec12d14
shumaikunkun <s1711520@gmail.com>
2019-10-16 00:36:58 +0900

    add C#

---
+++ hanoi.cs
@@ -0,0 +1,13 @@
+using System;
+public class Hanoi {
+  private void hanoi(int n,char x,char y,char z){
+    if(n>1) hanoi(n-1,x,z,y);
+    Console.WriteLine("{0}=>{1}"x,y);
+    if(n>1) hanoi(n-1,z,y,x);
+  }
+  public static void main() {
+    int n = int.Parse(Console.ReadLine());
+    Hanoi h = new Hanoi();
+    h.hanoi(n,'a','b','c');
+  }
+}
