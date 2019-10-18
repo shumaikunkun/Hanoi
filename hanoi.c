b686ff03c401e5024251892ba68968f3472b3b82
shumaikunkun <s1711520@gmail.com>
2019-10-18 15:47:09 +0900

    fix c, add gitignore

--- hanoi.c
+++ hanoi.c
@@ -1,13 +1,14 @@
-using System;
-public class Hanoi {
-  private void hanoi(int n,char x,char y,char z){
-    if(n>1) hanoi(n-1,x,z,y);
-    Console.WriteLine("{0}=>{1}",x,y);
-    if(n>1) hanoi(n-1,z,y,x);
-  }
-  public static void Main() {
-    int n = int.Parse(Console.ReadLine());
-    Hanoi h = new Hanoi();
-    h.hanoi(n,'a','b','c');
-  }
+#include <stdio.h>
+
+void hanoi(int n,char x,char y,char z){
+  if(n>1) hanoi(n-1,x,z,y);
+  printf("%c=>%c\n",x,y);
+  if(n>1) hanoi(n-1,z,y,x);
+}
+
+int main(void){
+  int n;
+  scanf("%d",&n);
+  hanoi(n,'a','b','c');
+  return 0;
 }
