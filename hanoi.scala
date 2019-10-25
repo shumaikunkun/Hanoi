dfd18380f6dcc7e0a8de2fcd9e3d24e12aa63493
shumaikunkun <s1711520@gmail.com>
2019-10-26 07:03:32 +0900

    fix Scala (add input function)

--- hanoi.scala
+++ hanoi.scala
@@ -1,7 +1,7 @@
 // This is Scala code
 object Main {
-	def main(args: Array[String]):Unit= hanoi(3,"a","b","c")
-	def hanoi(n:Int, x:String, y:String, z:String):Unit ={
+	def main(args: Array[String]):Unit = hanoi(readLine().toInt,"a","b","c")
+	def hanoi(n:Int, x:String, y:String, z:String):Unit = {
 		if(n>1) hanoi(n-1,x,z,y)
 		println(s"$x=>$y")
 		if(n>1) hanoi(n-1,z,y,x)
