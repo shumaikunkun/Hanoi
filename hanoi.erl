5790ff6d1568aa51bd242811b5390ac55dc7b812
shumaikunkun <s1711520@gmail.com>
2019-11-21 19:26:32 +0900

    add Erlang

---
+++ hanoi.erl
@@ -0,0 +1,13 @@
+% This is Erlang code
+
+-module(prog).
+-export([main/0]).
+
+main() ->
+	hanoi(3,"a","b","c").
+
+hanoi(1,X,_,Z) -> io:format("~s=>~s~n", [X, Z]);
+hanoi(N,X,Y,Z) when N > 0 ->
+    hanoi(N - 1, X, Z, Y),
+    io:format("~s=>~s~n", [X, Z]),
+    hanoi(N - 1, Y, X, Z).
