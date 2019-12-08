0af0f5f0fc074e97e9e8461fc4193cb42bcfbd9a
shumaikunkun <s1711520@gmail.com>
2019-12-08 14:29:43 +0900

    fix LISP

--- hanoi.lisp
+++ hanoi.lisp
@@ -1,1 +1,3 @@
-(defun hanoi(n x y z)(cond((> n 1)(hanoi(1- n) x z y)))(format t "~A -> ~A~1%" x y)(cond((> n 1)(hanoi(1- n) z y x))))(hanoi(read nil nil) 'a 'b 'c)
+; This is LISP code
+(defun hanoi(n x y z)(cond((> n 1)(hanoi(1- n) x z y)))(format t "~A -> ~A~1%" x y)(cond((> n 1)(hanoi(1- n) z y x))))
+(hanoi(read nil nil) 'a 'b 'c)
